c0506cdf7feba8093bcae73195453f019f3b5af7
Isak Mladenoff <imladenoff@gmail.com>
2017-06-08 13:16:05 -0700

    Initial commit

---
+++ bsearch.rb
@@ -0,0 +1,17 @@
+def bsearch(nums, target)
+  # nil if not found; can't find anything in an empty array
+  return nil if nums.empty?
+
+  probe_index = nums.length / 2
+  case target <=> nums[probe_index]
+  when -1
+    # search in left
+    bsearch(nums.take(probe_index), target)
+  when 0
+    probe_index # found it!
+  when 1
+    # search in the right; don't forget that the right subarray starts
+    # at `probe_index + 1`, so we need to offset by that amount.
+    sub_answer = bsearch(nums.drop(probe_index + 1), target)
+    (sub_answer.nil?) ? nil : (probe_index + 1) + sub_answer
+  end

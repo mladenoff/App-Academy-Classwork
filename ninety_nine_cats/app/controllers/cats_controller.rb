b38d50a7c32b4c22251a10872cf46e2847cbb347
Isak Mladenoff <imladenoff@gmail.com>
2017-06-20 16:54:14 -0700

    Create form partial for cats

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -28,9 +28,23 @@ class CatsController < ApplicationController
     end
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+    if cat.update(cat_params)
+      redirect_to cat_url(params[:id])
+    else
+      redirect_to edit_cat(params[:id])
+    end
+  end
+
   private
 
   def cat_params
-    params.require(:cat).permit(:name, :birth_date, :sex, :color)
+    params.require(:cat).permit(:name, :birth_date, :sex, :color, :description)
   end
 end

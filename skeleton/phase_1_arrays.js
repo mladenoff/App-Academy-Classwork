b0bf8d9d8c6817ed7826b861882def49caf19cee
Isak Mladenoff <imladenoff@gmail.com>
2017-06-28 12:24:27 -0700

    Initial commit

---
+++ skeleton/phase_1_arrays.js
@@ -0,0 +1,34 @@
+Array.prototype.uniq = function() {
+  let resultArray = [];
+  for (var i = 0; i < this.length; i++) {
+    if(!resultArray.includes(this[i])) {
+      resultArray.push(this[i]);
+    }
+  }
+  return resultArray;
+};
+
+// console.log([1,2,2,3].uniq());
+
+Array.prototype.twoSum = function(){
+  let resultArray = [];
+  for (var i = 0; i < this.length; i++) {
+    for (var j = i; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        resultArray.push([i,j]);
+      }
+    }
+  }
+  return resultArray;
+};
+
+Array.prototype.transpose = function() {
+  let resultArray = [];
+
+  for (var i = 0; i < this.length; i++) {
+    for (var j = 0; j < this[i].length; j++) {
+      
+    }
+  }
+
+};
